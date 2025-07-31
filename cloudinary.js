905fd931dd934a669e90f31ba7a17cbd27613f4b
Somnath <1628.sk46@gmail.com>
2025-07-31 14:55:10 +0530

    Updated toggle buttom

--- cloudinary.js
+++ cloudinary.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: 'wanderlust_DEV', // or your intended folder
-    allowed_formats: ['jpeg', 'png', 'jpg'],
+    allowed_formats: ['jpeg', 'png', 'jpg','avif'],
   },
 });
 
